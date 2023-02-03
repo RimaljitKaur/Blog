b09ca3e0733d94cd613bc082235a9f03dcd3a2b1
RimaljitKaur <86721110+RimaljitKaur@users.noreply.github.com>
2023-02-03 18:25:12 +0530

    Add files via upload

--- blog/blogonline/models.py
+++ blog/blogonline/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
 class Blog(models.Model):
     title = models.CharField(max_length=200,unique=True)
-    published_on = models.DateField(auto_now=True)
+    published_on = models.DateField(auto_now=False)
     description = models.CharField(max_length=200,unique=True)
-    slug = models.SlugField(unique=True, max_length=300)
+    slug = models.SlugField(max_length=300, null=True)
     content = models.TextField()
+    tags = TaggableManager()
 
-    def __str__(self) -> str:
-        return self.title
+    # def get_year(self):
+    #     return self.published_on.year
+
+    #def __str__(self) -> str:
+     #   return self.title
